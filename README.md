a4a753dfe1c551e82249112bc5ac5d14e57a5eb9
Oliver Facon <omf11@georgetown.edu>
2020-02-24 22:27:29 -0500

    Final Version

--- README.md
+++ README.md
@@ -1,1 +1,32 @@
 # robo-advisor
+
+## Installation
+Fork it, clone it, and choose a place to save it such as Desktop:
+```sh
+cd ~/Desktop/shopping-cart
+```
+
+## Setup
+No Packages to install
+Create a virtual environment named 'shopping' for example
+```sh
+conda create -n robo-advisor-env python=3.7
+```
+To activate the environment:
+```sh
+conda activate robo-advisor-env
+```
+The code should also work on base environment
+
+## Usage
+To run the script:
+```sh
+python robo_advisor.py
+```
+
+## Install Packages
+
+Make sure to install packages:
+```sh
+pip install -r requirements.txt
+```
--- app/robo_advisor.py
+++ app/robo_advisor.py
@@ -1,145 +1,128 @@
-import os
+# app/robo_advisor.py
 import csv
 import json
+import os
 import datetime
 import requests
 
-from dotenv import load_dotenv
-
-load_dotenv()
+import plotly
+import plotly.graph_objs as go
 
 def to_usd(price):
     return "${0:,.2f}".format(price)
 
-def process_urls():
-
-    response = requests.get(url)
-    parsed_response = json.loads(response.text) 
-    tsd = parsed_response["Time Series (Daily)"]
-
-
-    dates = list(tsd.keys())
-
-    latest_day = dates[0]
-
-    latest_close = tsd[latest_day]["4. close"]
-
-    high_prices = []
-    low_prices = []
-    #closing_prices = []
-
-    for date in dates:
-        high_price = tsd[date]["2. high"]
-        low_price = tsd[date]["3. low"]
-        #closing_price = tsd[date]["4. close"]
-        high_prices.append(high_price)
-        low_prices.append(low_price)
-        #closing_prices.append(closing_price)
-
-    recent_high = max(high_prices)
-    recent_low = min(low_prices)
-
-    if float(latest_close) < 1.2*float(recent_low) and float(latest_close) > 0.8*float(recent_high):
-        recommendation = "NO RECOMMENDATION"
-        reasoning = "CANNOT ACCURATELY ESTIMATE IF STOCK IS UNDERVALUED OR OVERVALUED. LATEST CLOSING PRICE IS WITHIN 20% OF THE RECENT HIGH AND LOW." 
-    elif float(latest_close) < 1.2*float(recent_low):
-        recommendation = "BUY!"
-        reasoning = "THE STOCK IS LIKELY TO BE UNDERVALUED (THE CLOSING PRICE IS WITHIN 20% OF THE STOCK'S RECENT LOW)"
-    elif float(latest_close) > 0.8*float(recent_high):
-        recommendation = "SELL!"
-        reasoning = "THE STOCK IS LIKELY TO BE OVERVALUED (THE CLOSING PRICE IS WITHIN 20% OF THE STOCK'S RECENT HIGH)"
-    else:
-        recommendation = "NO RECOMMENDATION"
-        reasoning = "NO SPECIFIC DATA TO ESTIMATE FUTURE PERFORMANCE"
-
-    print_info = {"symbol": symbols[count-1].upper(), "t_request": request_time, "t_refreshed": last_refreshed, "close": latest_close, "high": recent_high, "low": recent_low, "recommend": recommendation, "reason": reasoning}
-    print_stocks(print_info)
-
-def print_stocks(print_info):
-    print("-------------------------")
-    print("SELECTED SYMBOL: " + print_info["symbol"])
-    print("-------------------------")
-    print("REQUESTING STOCK MARKET DATA...")
-    print("REQUEST AT: " + print_info["t_request"])
-    print("-------------------------")
-    print("LASTEST DAY: " + print_info["t_refreshed"])
-    print("LATEST CLOSE: " + to_usd(float(print_info["close"])))
-    print("RECENT HIGH: " + to_usd(float(print_info["high"])))
-    print("RECENT LOW: " + to_usd(float(print_info["low"])))
-    print("-------------------------")
-    print("RECOMMENDATION: " + print_info["recommend"])           
-    print("RECOMMENDATION REASON: " + print_info["reason"])    
-    print("-------------------------")
-    print("WRITING DATA TO CSV...")
-    print("-------------------------")
-    if count == ticker_count:
-        print("HAPPY INVESTING!")
-    else:
-        print("-------------------------")
-        print("-------------------------")
-        print("-------------------------")
-    print("-------------------------")
-
-def to_csv():
-    csv_file_name = symbols[count-1].lower() + "_prices.csv"
-    csv_file_path = os.path.join(os.path.dirname(__file__), "..", "data", csv_file_name)
-
-    csv_headers = ["timestamp", "open",  "high", "low", "close","volume"]
-
-    with open(csv_file_path, "w") as csv_file: # "w" means "open the file for writing"
-        writer = csv.DictWriter(csv_file, fieldnames=csv_headers)
-        writer.writeheader() # uses fieldnames set above
-
-        for date in dates:
-            daily_prices = tsd[date]
-            writer.writerow({
-                csv_headers[0]: date,
-                csv_headers[1]: to_usd(float(daily_prices["1. open"])),
-                csv_headers[2]: to_usd(float(daily_prices["2. high"])),
-                csv_headers[3]: to_usd(float(daily_prices["3. low"])),
-                csv_headers[4]: to_usd(float(daily_prices["4. close"])),
-                csv_headers[5]: daily_prices["5. volume"]
-            })
-
-# MAIN BLOCK
-##########################################
-symbol_error = True
-symbols = []
-request_urls = []
-count = 0
-
-api_key = os.environ.get("api_key_env")
-
 while True:
-    symbol = input("Please enter a ticker number (type 'done' to load stocks): ").upper()
+    symbol = input("Please enter a stock symbol: ").upper()
     
     if symbol == "DONE":
         break
-
+    
     request_time = str(datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
 
-    request_url = f"https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol={symbol}&apikey={api_key}"
-
-    response = requests.get(request_url)
    
-    test_parsed_response = json.loads(response.text)
+    api_key = os.environ.get("api_key_env")
 
-    print(test_parsed_response["Meta Data"]["1. Information"])
+    request_url = f"https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol={symbol}&outputsize=full&apikey={api_key}"
+
     
+    response = requests.get(request_url)
+
+    
+    parsed_response = json.loads(response.text)
 
     try:
-        invalid_ticker_test = test_parsed_response["Meta Data"]["1. Information"] #testing whether I can access dictionary (check if ticker is vaiid)
-        symbols.append(symbol)
-        request_urls.append(request_url)
+        last_refreshed = parsed_response["Meta Data"]["3. Last Refreshed"]
+        break
     except:
         print("INVALID STOCK SYMBOL. PLEASE TRY AGAIN.")
 
 
-last_refreshed = test_parsed_response["Meta Data"]["3. Last Refreshed"]
 
-ticker_count = len(request_urls)
+tsd = parsed_response["Time Series (Daily)"]
 
-for url in request_urls:
-    count += 1
-    process_urls()
-    
+dates = list(tsd.keys())
+
+latest_day = dates[0]
+
+latest_close = tsd[latest_day]["4. close"]
+
+high_prices = []
+low_prices = []
+closing_prices = []
+
+for date in dates:
+    high_price = tsd[date]["2. high"]
+    low_price = tsd[date]["3. low"]
+    closing_price = tsd[date]["4. close"]
+    high_prices.append(high_price)
+    low_prices.append(low_price)
+    closing_prices.append(closing_price)
+
+recent_high = max(high_prices[0:100])
+recent_low = min(low_prices[0:100])
+year_high = max(high_prices[0:252])
+year_low = min(low_prices[0:252])
+
+if float(latest_close) < 1.2*float(recent_low) and float(latest_close) > 0.8*float(recent_high):
+    recommendation = "NO RECOMMENDATION"
+    reasoning = "CANNOT ACCURATELY ESTIMATE IF STOCK IS UNDERVALUED OR OVERVALUED. LATEST CLOSING PRICE IS WITHIN 20% OF THE RECENT HIGH AND LOW." 
+elif float(latest_close) < 1.2*float(recent_low):
+    recommendation = "BUY!"
+    reasoning = "THE STOCK IS LIKELY TO BE UNDERVALUED (THE CLOSING PRICE IS WITHIN 20% OF THE STOCK'S RECENT LOW)"
+elif float(latest_close) > 0.8*float(recent_high):
+    recommendation = "SELL!"
+    reasoning = "THE STOCK IS LIKELY TO BE OVERVALUED (THE CLOSING PRICE IS WITHIN 20% OF THE STOCK'S RECENT HIGH)"
+else:
+    recommendation = "NO RECOMMENDATION"
+    reasoning = "NO SPECIFIC DATA TO ESTIMATE FUTURE PERFORMANCE"
+
+print("-------------------------")
+print(f"SELECTED SYMBOL: {symbol}")
+print("-------------------------")
+print("REQUESTING STOCK MARKET DATA...")
+print(f"REQUEST AT: "+ request_time)
+print("-------------------------")
+print(f"LATEST DAY: {last_refreshed}")
+print(f"LATEST CLOSE: {to_usd(float(latest_close))}")
+print(f"YEAR HIGH : {to_usd(float(year_high))}")
+print(f"YEAR LOW : {to_usd(float(year_low))}")
+print("-------------------------")
+print(f"RECOMMENDATION: {recommendation}")           
+print(f"RECOMMENDATION REASON: {reasoning}")    
+print("-------------------------")
+print("WRITING DATA TO CSV...")
+print("-------------------------")
+
+fig = go.Figure()
+
+fig.update_layout(          
+    title=f"{symbol} Stock Price (1yr)",
+    xaxis_title="Date",
+    yaxis_title="Stock Price (USD)",        #consulted https://plot.ly/python/figure-labels/ for all fig functions
+)
+
+fig.add_trace(go.Scatter(
+    x = dates[0:252],
+    y = closing_prices[0:252]
+))
+
+fig.show()
+
+csv_file_path = os.path.join(os.path.dirname(__file__), "..", "data", "prices.csv")
+
+csv_headers = ["timestamp", "open",  "high", "low", "close","volume"]
+
+with open(csv_file_path, "w") as csv_file: # "w" means "open the file for writing"
+    writer = csv.DictWriter(csv_file, fieldnames=csv_headers)
+    writer.writeheader() # uses fieldnames set above
+    for date in dates:
+        daily_prices = tsd[date]
+        writer.writerow({
+            csv_headers[0]: date,
+            csv_headers[1]: to_usd(float(daily_prices["1. open"])),
+            csv_headers[2]: to_usd(float(daily_prices["2. high"])),
+            csv_headers[3]: to_usd(float(daily_prices["3. low"])),
+            csv_headers[4]: to_usd(float(daily_prices["4. close"])),
+            csv_headers[5]: daily_prices["5. volume"]
+        })
+
